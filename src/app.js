f415eb0dda31d83c858b16cd57dd96f4ab87314d
Mythrex <shivam.kaushik73@gmail.com>
2018-04-28 23:08:30 +0530

    Add functionality for dynamic choosign a subject

--- src/app.js
+++ src/app.js
@@ -7,10 +7,15 @@ var gradesListObj;
 //jquery variables
 $gradeListAccordian = $('#grades-list-accordian');
 $gradesListTemplate = $('#grades-list-template');
+$questionCardTemplate =$('#question-card-template');
+$questionsContainer = $('#questions-container');
+$pageHeaderTemplate = $('#page-header-template');
+
 //templates here
 //templateFor Accordian
 var accordianTemplate = $gradesListTemplate.html();
-
+var questionTemplate = $questionCardTemplate.html();
+var pageHeaderTemplate = $pageHeaderTemplate.html();
 //curChapters
 var curChap,curSub,curGrade;
 
@@ -34,14 +39,7 @@ $(function(){
 		  
 
 		  //click event of chapter
-		  $('.chapters>li').click(function(event) {
-		  	$this = $(this);
-		  	remPrevActiveChap();
-		  	$this.addClass('active')
-		  	curChap = $this.data('chapter-id');
-		  	curSub = $this.parent().parent().parent().data('subject-id');
-		  	curGrade = $this.parent().parent().parent().parent().parent().parent().data('grade-id');
-		  });
+		  $('.chapters>li').click(toggleChapter);
   });
 
 });
@@ -59,15 +57,36 @@ function saveData(jsonData) {
 	localStorage.setItem('gradesList',JSON.stringify(jsonData));
 }
 
-function appendElements(array) {
-	var html = ejs.render(accordianTemplate,array);
-	$gradeListAccordian.append(html);
+function appendElements(obj) {
+	var accordianHtml = ejs.render(accordianTemplate,obj);
+	$gradeListAccordian.append(accordianHtml);
+	var questionsHtml = ejs.render(questionTemplate, obj);
+	$questionsContainer.append(questionsHtml);
+	var pageHeaderHtml = ejs.render(pageHeaderTemplate, {"curGrade": curGrade,"curSub": curSub,"curChap": curChap,"gradesList": obj.gradesList});
+	$('#page-header-container').html(pageHeaderHtml);
 }
 
 function remPrevActiveChap(){
-	var str = `.grade[data-grade-id="${curGrade}"] .subject[data-subject-id="${curSub}"] .chapter[data-chapter-id="${curChap}"]`;
-	$prevChap = $(str);
-	if($prevChap.length != 0)
-		$prevChap.removeClass('active')
-	console.log(curGrade);
+	var query = `.grade[data-grade-id="${curGrade}"] .subject[data-subject-id="${curSub}"] .chapter[data-chapter-id="${curChap}"]`;
+	$prevChap = $(query);
+	if($prevChap.length != 0){
+		$prevChap.removeClass('active');
+		//remove previous questions
+		$(`.${curGrade}-${curSub}-${curChap}`).hide();
+	}
+}
+
+function toggleChapter(event) {
+	$this = $(this);
+	remPrevActiveChap();
+	$this.addClass('active')
+	curChap = $this.data('chapter-id');
+	curSub = $this.parent().parent().parent().data('subject-id');
+	curGrade = $this.parent().parent().parent().parent().parent().parent().data('grade-id');
+	//show current questions
+	$(`.${curGrade}-${curSub}-${curChap}`).show();
+	$('#placeholder-text').hide();
+	//display page Header
+	var pageHeaderHtml = ejs.render(pageHeaderTemplate, {"curGrade": curGrade,"curSub": curSub,"curChap": curChap,"gradesList": gradesListObj.gradesList});
+	$('#page-header-container').html(pageHeaderHtml);
 }

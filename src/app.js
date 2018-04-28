486ad56b3ae19bb9c160bf2bfd36c038d51622d7
Mythrex <shivam.kaushik73@gmail.com>
2018-04-29 00:49:13 +0530

    Add support for Addition of Grades

--- src/app.js
+++ src/app.js
@@ -28,21 +28,21 @@ $(function(){
   
   $('.tooltipped').tooltip();
 
-  
+  refreshDoc();
   //main functions start here
-  $.getJSON('/data.json', function(dat, textStatus) {
-  		
-  		gradesListObj = dat;
-  		saveData(dat)
-  		refreshDoc();
-  		$('.collapsible').collapsible();	
+  
+});
+
+function inits() {
+	$('.collapsible').collapsible();	
 		  
 
 		  //click event of chapter
 		  $('.chapters>li').click(toggleChapter);
-  });
 
-});
+		  //click event for add Grade
+		  $('#btn-grade').click(addGrade);
+}
 
 function refreshDoc() {
 	fetchData();
@@ -51,6 +51,12 @@ function refreshDoc() {
 
 function fetchData() {
 	gradesListObj = JSON.parse(localStorage.getItem('gradesList'));
+	if(gradesListObj == undefined){
+		$.getJSON('/data.json', function(dat, textStatus) {
+				gradesListObj = dat;
+				saveData(dat)
+		});
+	}
 }
 
 function saveData(jsonData) {
@@ -59,11 +65,12 @@ function saveData(jsonData) {
 
 function appendElements(obj) {
 	var accordianHtml = ejs.render(accordianTemplate,obj);
-	$gradeListAccordian.append(accordianHtml);
+	$gradeListAccordian.html(accordianHtml);
 	var questionsHtml = ejs.render(questionTemplate, obj);
-	$questionsContainer.append(questionsHtml);
+	$questionsContainer.html(questionsHtml);
 	var pageHeaderHtml = ejs.render(pageHeaderTemplate, {"curGrade": curGrade,"curSub": curSub,"curChap": curChap,"gradesList": obj.gradesList});
 	$('#page-header-container').html(pageHeaderHtml);
+	inits();
 }
 
 function remPrevActiveChap(){
@@ -89,4 +96,13 @@ function toggleChapter(event) {
 	//display page Header
 	var pageHeaderHtml = ejs.render(pageHeaderTemplate, {"curGrade": curGrade,"curSub": curSub,"curChap": curChap,"gradesList": gradesListObj.gradesList});
 	$('#page-header-container').html(pageHeaderHtml);
+}
+
+function addGrade(e) {
+	console.log('hello');
+	var $inputGrade = $('#input-grade')
+	var value = $inputGrade.val();
+	gradesListObj.gradesList.push({name: value,subjectList: []});
+	saveData(gradesListObj);
+	refreshDoc();
 }
